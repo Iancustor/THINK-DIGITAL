9f0dd92396f1660901a3c9ba115999b058a9cd0d
Ian Sebugenyi <140151921+Iancustor@users.noreply.github.com>
2023-12-15 00:59:02 +0300

    Initial commit

---
+++ Assets/Js/typed.js
@@ -0,0 +1,8 @@
+import Typed from "typed.js";
+const typed = new Typed(".welcome", {
+  strings: [
+    "Think digital is your one-stop destination for digital content creation, Marketing,Web development,Photography,Videography and social Media ",
+    "Partner with us for an Innovative and comprehensive digital experiance.",
+  ],
+  typeSpeed: 55,
+});
