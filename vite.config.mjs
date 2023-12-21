e496fb0b873444a6da6dd150ef42ad64b190bd57
Ian Sebugenyi <140151921+Iancustor@users.noreply.github.com>
2023-12-21 10:04:29 +0300

    update

--- vite.config.mjs
+++ vite.config.mjs
@@ -6,7 +6,6 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         about: resolve(__dirname, "about.html"),
-        blogs: resolve(__dirname, "blog.html"),
         contact: resolve(__dirname, "contact.html"),
         services: resolve(__dirname, "services.html"),
       },
