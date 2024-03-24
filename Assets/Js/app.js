a7e269cfb4438d85b601290f3b1d34be98d2d519
Ian Sebugenyi <140151921+Iancustor@users.noreply.github.com>
2024-03-24 21:26:56 +0300

    updated

--- Assets/Js/app.js
+++ Assets/Js/app.js
@@ -76,3 +76,35 @@ const year = today.getFullYear();
 // curent year
 const yearContainer = document.querySelector(".year");
 yearContainer.textContent = year;
+
+// dark Mode
+const darkModeBtn = document.querySelector(".darkmode-toggle");
+// console.log(darkModeBtn);
+const body = document.body;
+darkModeBtn.addEventListener("click", changeMode);
+const nameEl = document.querySelector(".name");
+
+// adding theme to local storage
+let theme = localStorage.getItem("theme");
+// console.log(theme);
+theme === "light" ? enableLightMode() : enableDarkMode();
+
+function changeMode() {
+  theme === "light" ? enableDarkMode() : enableLightMode();
+  body.classList.contains("dark")
+    ? (darkModeBtn.innerHTML = "<i class='bx bxs-sun'></i>")
+    : (darkModeBtn.innerHTML = "<i class='bx bxs-moon'></i>");
+}
+function enableLightMode() {
+  body.classList.remove("dark");
+  theme = "light";
+  localStorage.setItem("theme", "light");
+}
+
+function enableDarkMode() {
+  body.classList.add("dark");
+  theme = "dark";
+  localStorage.setItem("theme", "dark");
+}
+
+// console.log(theme)
